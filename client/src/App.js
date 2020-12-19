33e737af260d25031602ee99e928d9c0c9cea91f
Chana96 <ahasithachandana@gmail.com>
2020-12-20 01:57:58 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        <h1>Product List</h1>
+      </main>
+      <footer> All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
